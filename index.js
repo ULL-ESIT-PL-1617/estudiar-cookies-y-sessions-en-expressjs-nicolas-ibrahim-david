f84675a9acd035717dc23c29e8e6f4975a89ffe8
alu0100826166 <alu0100826166@ull.edu.es>
2017-03-13 22:34:52 +0000

    Arreglando despliegue en heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/docs'));
 
 app.get('/', function(req, res) {
-  res.redirect('./docs/_book/');
+  res.redirect('/_book/');
 });
 
 app.listen(app.get('port'), function() {
