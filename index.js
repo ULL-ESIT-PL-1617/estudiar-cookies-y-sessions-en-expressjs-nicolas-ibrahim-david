aa55e13afc6336cc8a3ecbcc8df69aaa6146458c
alu0100826166 <alu0100826166@ull.edu.es>
2017-03-13 23:26:53 +0000

    ...

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var express     = require('express');
 var app         = express();
+var path        = require('path');
+
+process.env.PWD = process.cwd();
+
+app.set('views', path.join(process.env.PWD, 'public'));
+
+app.use(express.static(path.join(process.env.PWD, '_book')));
+
 
 // Establezco el directorio de trabajo
-process.env.PWD = process.cwd();
 
 // Puerto de escucha de la app
 app.set('port', (process.env.PORT || 5000));
 
 // Rutas para servir ficheros estaticos
-app.use(express.static(process.env.PWD + '/docs'));
-app.use(express.static(process.env.PWD + '/_book'));
-app.use(express.static(process.env.PWD + '/src'));
+// app.use(express.static(process.env.PWD + '/docs'));
+// app.use(express.static(process.env.PWD + '/_book'));
+// app.use(express.static(process.env.PWD + '/src'));
 
 // Manejador para la raiz
 // app.get('/', function(req, res) {
