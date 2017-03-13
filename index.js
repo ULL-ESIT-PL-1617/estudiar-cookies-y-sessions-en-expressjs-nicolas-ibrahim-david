3de896d43a40b33e36a443ff5f7bd24c38cc1c4b
alu0100826166 <alu0100826166@ull.edu.es>
2017-03-13 23:18:36 +0000

    Arreglando despliegue en heroku

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var app = express();
+var express     = require('express');
+var app         = express();
 
+// Establezco el directorio de trabajo
+process.env.PWD = process.cwd();
+
+// Puerto de escucha de la app
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/docs'));
-app.use(express.static(__dirname));
+// Rutas para servir ficheros estaticos
+app.use(express.static(process.env.PWD + '/docs'));
+app.use(express.static(process.env.PWD + '/_book'));
+app.use(express.static(process.env.PWD + '/src'));
 
-app.get('/', function(req, res) {
-  res.send('Hola Heroku');
-});
+// Manejador para la raiz
+// app.get('/', function(req, res) {
+//   res.send('Hola Heroku');
+// });
 
+// Escucha de la app
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
