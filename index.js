039f68d35b55b681f286b556c8ff9410886d2fce
alu0100826166 <alu0100826166@ull.edu.es>
2017-03-14 17:33:16 +0000

    Terminando

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ process.env.PWD = process.cwd();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(path.join(process.env.PWD, '_book')));
+app.use(express.static(path.join(process.env.PWD, '/public')));
 
-app.get('/home', function(req, res) {
+app.get('/', function(req, res) {
     res.send('<html><body><a href="index.html">Enlace al libro</a></body></html>');
 });
 
